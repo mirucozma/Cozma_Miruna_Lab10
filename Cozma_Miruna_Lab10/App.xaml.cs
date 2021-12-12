02ccdeea5fd2ace20afcc4fdea5b08c790dbe793
Miruna Cozma <bianca.mirunacozma@gmail.com>
2021-12-12 17:51:34 +0200

    Add project files.

---
+++ Cozma_Miruna_Lab10/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Cozma_Miruna_Lab10.Data;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Cozma_Miruna_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+
+           // MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
